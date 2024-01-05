40d0ddd46ac3c651881dbbe05220d18601691ecd
Egito Filho <egitofilho4@gmail.com>
2024-01-04 22:32:59 -0300

    create a load state

--- app/src/main/java/com/example/marvelapp/features/heroes/presentation/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/features/heroes/presentation/CharactersFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
 import com.example.marvelapp.databinding.FragmentCharactersBinding
 import com.example.marvelapp.features.heroes.presentation.adapter.CharacterAdapter
+import com.example.marvelapp.features.heroes.presentation.adapter.CharacterLoadStateAdapter
 import com.example.marvelapp.features.heroes.presentation.viewmodel.CharactersViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -18,7 +24,7 @@ class CharactersFragment : Fragment() {
 
     private var _binding: FragmentCharactersBinding? = null
     private val binding: FragmentCharactersBinding get() = _binding!!
-    private val charactersAdapter = CharacterAdapter()
+    private lateinit var charactersAdapter: CharacterAdapter
     private val viewModel: CharactersViewModel by viewModels()
 
     override fun onCreateView(
@@ -36,25 +42,69 @@ class CharactersFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initCharactersAdapter()
         collectCharacters()
+        observeInitialLoadState()
     }
 
     private fun initCharactersAdapter() {
+        charactersAdapter = CharacterAdapter()
         binding.recyclerCharacters.run {
             setHasFixedSize(true)
-            adapter = charactersAdapter
+            adapter = charactersAdapter.withLoadStateFooter(
+                footer =  CharacterLoadStateAdapter(
+                charactersAdapter::retry
+            ))
         }
     }
 
     private fun collectCharacters() {
         lifecycleScope.launch {
-            viewModel.charactersPagingData("").collect{ pagingData ->
-                charactersAdapter.submitData(pagingData)
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.charactersPagingData("").collect{ pagingData ->
+                    charactersAdapter.submitData(pagingData)
+                }
             }
+        }
+    }
+
+    private fun observeInitialLoadState() {
+        lifecycleScope.launch {
+            charactersAdapter.loadStateFlow.collectLatest { loadState ->
+                binding.flipperCharacters.displayedChild = when(loadState.refresh) {
+                    is LoadState.Loading -> {
+                        setShimmerVisibility(true)
+                        FLIPPER_CHILD_LOADING
+                    }
+                    is LoadState.NotLoading -> {
+                        setShimmerVisibility(false)
+                        FLIPPER_CHILD_CHARACTERS
+                    }
+                    is LoadState.Error -> {
+                        setShimmerVisibility(false)
+                        binding.includeViewCharactersErrorState.buttonRetry.setOnClickListener {
+                            charactersAdapter.refresh()
+                        }
+                        FLIPPER_CHILD_ERROR
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setShimmerVisibility(visibility: Boolean) {
+        binding.includeViewCharactersLoadingState.shimmerCharacters.run {
+            isVisible = visibility
+            if(visibility) startShimmer() else startShimmer()
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        private const val FLIPPER_CHILD_LOADING = 0
+        private const val FLIPPER_CHILD_CHARACTERS = 1
+        private const val FLIPPER_CHILD_ERROR = 2
     }
 }

7277b600d12cfcb452791ff82b04e819f6d0e976
J Egito GS Filho <egitofilho4@gmail.com>
2024-02-14 11:34:02 -0300

    refactor paging source and tests

--- app/src/main/java/com/example/marvelapp/features/heroes/data/paging/CharactersPagingSource.kt
+++ app/src/main/java/com/example/marvelapp/features/heroes/data/paging/CharactersPagingSource.kt
@@ -10,29 +10,37 @@ class CharactersPagingSource(
     private val remoteDataSource: CharactersRemoteDataSource,
     private val query: String
 ): PagingSource<Int, CharacterEntity>() {
+
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CharacterEntity> {
-        return try {
+        return try{
+
             val offset = params.key ?: 0
+
             val queries = hashMapOf(
                 "offset" to offset.toString()
             )
-            if(query.isNotEmpty()){
+
+            if (query.isNotEmpty()) {
                 queries["nameStartsWith"] = query
             }
 
-            val charactersPaging = remoteDataSource.fetchCharacters(queries)
-            val responseOffset = charactersPaging.offset
-            val totalCharacters = charactersPaging.total
+            val characterPaging = remoteDataSource.fetchCharacters(queries)
+
+            val responseOffset = characterPaging.offset
+            val totalCharacters = characterPaging.total
 
             LoadResult.Page(
-                data = charactersPaging.characters,
+                data = characterPaging.characters,
                 prevKey = null,
-                nextKey = if(responseOffset < totalCharacters) responseOffset + LIMIT else null
+                nextKey = if (responseOffset < totalCharacters) {
+                    responseOffset + LIMIT
+                } else null
             )
-        } catch (exception: Exception){
+        } catch (exception: Exception) {
             LoadResult.Error(exception)
         }
     }
+
     override fun getRefreshKey(state: PagingState<Int, CharacterEntity>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
